b1542a56487c2705b50fa74468a8f1f013518d4c
leejunseok <wnstjr120422@naver.com>
2023-10-24 04:16:27 +0900

    fix: 메시지 최소 길이, 최대길이 설정

--- src/main/java/io/junrock/GAZA/domain/message/dto/MessageDto.java
+++ src/main/java/io/junrock/GAZA/domain/message/dto/MessageDto.java
@@ -4,10 +4,17 @@ import io.junrock.GAZA.domain.member.entity.Member;
 import io.junrock.GAZA.domain.message.entity.Message;
 import lombok.Builder;
 import lombok.Getter;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Getter
 @Builder
 public class MessageDto {
+    @NotBlank
+    @Size(min = 3,max = 100)
     private String content;
 
     public Message toEntity(Member member) {
