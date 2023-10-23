5344113e60eeca1ba26f370a2182f1132b75bd54
leejunseok <wnstjr120422@naver.com>
2023-10-24 03:40:05 +0900

    feat: Spring 설정 추가

---
+++ src/main/java/io/junrock/GAZA/GazaApplication.java
@@ -0,0 +1,15 @@
+package io.junrock.GAZA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class GazaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GazaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/junrock/GAZA/GazaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.junrock.GAZA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GazaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
