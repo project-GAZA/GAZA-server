85be291cfb9fdd096b50c74110b150d21ca10249
leejunseok <wnstjr120422@naver.com>
2023-11-28 14:27:48 +0900

    feat: Add 기부타입

---
+++ src/main/java/io/junrock/GAZA/domain/message/dto/TypeMessage.java
@@ -0,0 +1,12 @@
+package io.junrock.GAZA.domain.message.dto;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TypeMessage {
+    public static final String CAUTION="alert";
+    public static final String LIKE="like";
+
+    public static final String MESSAGE="message";
+    public static final String DONATE="donate";
+}
