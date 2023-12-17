39e29280b7a54eee714b9a381dbe3b3d83f13364
leejunseok <wnstjr120422@naver.com>
2023-12-18 00:05:58 +0900

    delete: NEW키워드

--- src/main/java/io/junrock/GAZA/domain/message/dto/TypeMessage.java
+++ src/main/java/io/junrock/GAZA/domain/message/dto/TypeMessage.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Configuration;
 public class TypeMessage {
     public static final String CAUTION="alert";
     public static final String LIKE="like";
-    public final static String NEW = "new";
 
     public static final String MESSAGE="message";
     public static final String DONATE="donate";
