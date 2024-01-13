2f8fded9c84c8e24fb6736eb0bc10634499b9b0a
leejunseok <wnstjr120422@naver.com>
2024-01-13 14:52:40 +0900

    feat : 메시지 타입 분류 DTO추가

---
+++ src/main/java/io/junrock/GAZA/domain/message/dto/MessageTypeDto.java
@@ -0,0 +1,11 @@
+package io.junrock.GAZA.domain.message.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+public class MessageTypeDto {
+    private String donateType;
+}
