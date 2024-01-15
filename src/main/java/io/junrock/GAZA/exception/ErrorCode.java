81787624949e90d8134a716649b6cb864f631bae
leejunseok <wnstjr120422@naver.com>
2024-01-15 20:30:45 +0900

    feat : 관리자 예외 처리 추가

--- src/main/java/io/junrock/GAZA/exception/ErrorCode.java
+++ src/main/java/io/junrock/GAZA/exception/ErrorCode.java
@@ -11,7 +11,9 @@ public enum ErrorCode {
     CONTAIN_BADWORD(HttpStatusCode.CONTAIN_BADWORD.getStatus(),"메시지에 욕설이 포함되어 있습니다!"),
     NOT_FOUND_MESSAGE(HttpStatusCode.NOT_FOUND_MESSAGE.getStatus(),"존재하지 않는 메시지입니다!"),
     EXIST_IP(HttpStatusCode.BAD_REQUEST.getStatus(),"이미 버튼을 누른 IP입니다!"),
-    NOT_FOUND_DONATE(HttpStatusCode.NOT_FOUND_MESSAGE.getStatus(),"존재하지 않는 기부금입니다!");
+    NOT_FOUND_DONATE(HttpStatusCode.NOT_FOUND_MESSAGE.getStatus(),"존재하지 않는 기부금입니다!"),
+    NOt_FOUND_ADMIN(HttpStatusCode.BAD_REQUEST.getStatus(), "존재하지 않는 관리자입니다!"),
+    NOT_EXIST_AUTHENTICATION(HttpStatusCode.UNAUTHORIZED.getStatus(), "Security Context에 인증 정보가 없습니다.");
     private final int status;
     private final String message;
 }
