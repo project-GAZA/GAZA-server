27e668ee4f9562de1799c493dc218759b9cdee24
leejunseok <wnstjr120422@naver.com>
2024-01-15 20:30:12 +0900

    feat : admin API 권한 설정 추가

--- src/main/java/io/junrock/GAZA/domain/donate/controller/DonateController.java
+++ src/main/java/io/junrock/GAZA/domain/donate/controller/DonateController.java
@@ -7,6 +7,7 @@ import io.junrock.GAZA.domain.donate.service.DonateService;
 import io.junrock.GAZA.domain.message.dto.PageRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -32,12 +33,16 @@ public class DonateController {
     }
 
     @GetMapping("/all")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
     public ResponseEntity<List<DonateResponseDto>> donateList(PageRequestDto pageRequestDto){
         return ResponseEntity.ok(donateService.getList(pageGenerate(pageRequestDto)));
     }
 
     @PatchMapping("/{donateId}")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
     public ResponseEntity<DonateResponseDto> modifyAmount(@PathVariable Long donateId,@RequestBody AmountDto amount){
         return ResponseEntity.ok(donateService.modifyDonate(donateId,amount));
     }
+
+
 }
