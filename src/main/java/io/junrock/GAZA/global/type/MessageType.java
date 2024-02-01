52b40b371c9db89c5442c6ffe65d61ebe0562fda
leejunseok <wnstjr120422@naver.com>
2024-02-01 15:47:38 +0900

    refactor : 패키지 변경

--- src/main/java/io/junrock/GAZA/global/type/MessageType.java
+++ src/main/java/io/junrock/GAZA/global/type/MessageType.java
@@ -1,11 +1,11 @@
-package io.junrock.GAZA.domain.message.dto;
+package io.junrock.GAZA.global.type;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public enum TypeMessage {
+public enum MessageType {
     MESSAGE("message"),
     DONATE("donate");
     private String donateType;
