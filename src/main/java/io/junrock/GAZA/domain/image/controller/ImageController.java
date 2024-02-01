60a7684514e56c1813bf0060568957a5539add07
leejunseok <wnstjr120422@naver.com>
2024-02-01 16:48:16 +0900

    feat : Image엔티티 추가

--- src/main/java/io/junrock/GAZA/domain/image/controller/ImageController.java
+++ src/main/java/io/junrock/GAZA/domain/image/controller/ImageController.java
@@ -6,6 +6,7 @@ import io.junrock.GAZA.domain.image.service.ImageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -19,17 +20,20 @@ public class ImageController {
     private final ImageService imageService;
 
     @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
     public void uploadImage(@RequestPart(required = false) ImageRequestDto requestDto,
                             @RequestPart(required = false) MultipartFile file) {
         imageService.saveImage(requestDto, file);
     }
 
     @DeleteMapping("/{imageId}")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
     public void deleteImage(@PathVariable Long imageId){
         imageService.delete(imageId);
     }
 
     @GetMapping
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
     public ResponseEntity<List<ImageResponseDto>> imageList(){
         return ResponseEntity.ok(imageService.getImages());
     }
